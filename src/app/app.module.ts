e8697dc49cce65df57c7c7c244163309692b026c
Ian <Iokeef@gmail.com>
2017-05-02 19:09:27 -0400

    Add detail functionality, preliminary router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,34 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { RouterModule }  from '@angular/router';
 
 import { AppComponent }  from './app.component';
+
+RouterModule.forRoot([
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  }
+])
 
 @NgModule({
   imports:      [
     BrowserModule,
     FormsModule,
    ],
-  declarations: [ AppComponent ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    HeroesComponent,
+   ],
+   providers: [
+    HeroService
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
