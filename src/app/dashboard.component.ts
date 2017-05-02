c02732d2d6d17ae4ae1bca11bd57983dce3b4abc
Ian <Iokeef@gmail.com>
2017-05-02 19:30:28 -0400

    add oninit and top heroes to dash

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroService }              from './hero.service';
+import { Hero } from  './hero';
 
 @Component({
   selector: 'my-dashboard',
   templateUrl: './dashboard.component.html'
 })
 
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
+  constructor(private heroService: HeroService) {
+  }
 
+  ngOnInit(): void {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(0,4))
+  }
 }
