89a1685528e6144662b640370335a7165d19024d
Ian <Iokeef@gmail.com>
2017-06-29 12:19:55 -0400

    Fix repo

--- calcprob.rb
+++ calcprob.rb
@@ -2,15 +2,21 @@ def calculatorParse(str)
   operands = str.split("").select{ |char| char == "+" || char == "*"}
   regex = Regexp.new("[\+\*]")
   numbers = str.split(regex).map(&:to_i)
-  total = numbers[0]
+  started = false
+
+  total
+end
+
+def doMath|(num1, num2, &blk)
   operands.each.with_index do |operand, idx|
     if operand == "*"
-      total *= numbers[idx+1]
-    else
-      total += numbers[idx+1]
+      unless started
+        started = true
+        total = numbers[idx]
+      end
+      blk.call(num1, num2)
     end
   end
-  total
 end
 
 str = "53+10*20+4"
